6d063031e9bb6631e392fc3eb6abc7d495295e58
Davut Kurt <51950119+davutkurt@users.noreply.github.com>
2019-06-19 20:00:01 +0300

    Create homework4.sql

---
+++ homework4.sql
@@ -0,0 +1,22 @@
+SET SERVEROUTPUT ON;
+DECLARE
+  intern_name VARCHAR(50):='Davut';
+  BEGIN
+      SELECT university_name INTO intern_name FROM intern WHERE first_name = intern_name;
+      DBMS_OUTPUT.PUT_LINE(intern_name);
+END;
+
+
+
+SET SERVEROUTPUT ON;
+DECLARE
+  temp VARCHAR(50);
+  intern_name VARCHAR(50):='Davut';
+    PROCEDURE find_university(temp IN OUT VARCHAR) IS
+    BEGIN
+      SELECT university_name INTO temp FROM intern WHERE first_name = temp;
+      DBMS_OUTPUT.PUT_LINE(temp);
+    END; 
+BEGIN
+  find_university(intern_name);
+END;
