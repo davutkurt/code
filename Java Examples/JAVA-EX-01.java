dce14532c36b5ee782a9ddfee57d8b9d5eb8e2ae
Davut Kurt <51950119+davutkurt@users.noreply.github.com>
2019-06-23 19:23:09 +0300

    Create JAVA-EX-01.java

---
+++ Java Examples/JAVA-EX-01.java
@@ -0,0 +1,28 @@
+package duplicate;
+
+public class Prime {
+
+	public static void main(String[] args) {
+		
+			System.out.println("Prime numbers that between 1 and 20 are :");
+			boolean b=true; int i=0; int count=0;
+			
+			for( i=2; i<=20; i++ ){
+				b = true;
+				
+				for (  count=2; count<=(i/2); count++ ){
+					
+					if( i%count == 0 ){
+						b = false;
+						break;
+					}
+				}
+				if ( b==true ){
+					System.out.println( i );
+				}
+			}
+	}
+}
+	
+
+
