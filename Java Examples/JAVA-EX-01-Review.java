93efcd79e71c86d07811586f9de11ea511064dff
Davut Kurt <51950119+davutkurt@users.noreply.github.com>
2019-06-27 19:37:18 +0300

    Update JAVA-EX-01-Review.java

--- Java Examples/JAVA-EX-01-Review.java
+++ Java Examples/JAVA-EX-01-Review.java
@@ -5,27 +5,39 @@ public class PrimeNumber {
 	private void printMessage(String print) {
 		System.out.println(print);
 	}
-	private void findPrimeNumbers(){
+	
+	public boolean functionMod(int inputNumber, int modValue) {
 		
-		for(int i=2; i<=20; i++ ){
-			boolean isPrime = true;
-			
-			for (int count=2; count<=(i/2); count++ ){
-				
-				if( i % count == 0 ){
-					isPrime = false;
-					break;
-				}
+		if( inputNumber % modValue == 0 ) {
+			return true;
 			}
-			if ( isPrime ){
-				printMessage(""+ i );
+		return false;
+	}
+	
+	public boolean isPrime( int parameter ) {
+		
+		for ( int count=2; count<=(parameter/2); count++ ){
+			
+			if( functionMod(parameter, count) ){
+				return false;	
 			}
 		}
+		
+		return true;
 	}
+	private void findPrimeNumbers(int inputNumber){
+		
+		for(int i=2; i<=inputNumber; i++ ){
+			
+			if( isPrime( i ) )
+				printMessage(""+i);						
+		}
+	}
+	
 	public static void main(String[] args) {
 			PrimeNumber primeNumber = new PrimeNumber();
 			primeNumber.printMessage("Prime numbers that between 1 and 20 are :");
-			primeNumber.findPrimeNumbers();
+			primeNumber.findPrimeNumbers(20);
 
 	}
 }
